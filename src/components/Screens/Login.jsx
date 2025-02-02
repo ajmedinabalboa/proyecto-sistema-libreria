488ae3b65a2338b5b752e8ef2198f2d31463e83d
RogersSC <ssras@gmail.com>
2025-02-02 16:08:38 -0400

    avance hasta Login

--- src/components/Screens/Login.jsx
+++ src/components/Screens/Login.jsx
@@ -1,7 +1,9 @@
+import FormLogin from "../Forms/FomLogin.jsx";
 const Login = () => {
     return (
         <div>
-            <h1>Login</h1>
+          
+            <FormLogin titleForm={"Bienvenido a la Libreria G2 React"}/>
         </div>
     )
 }
