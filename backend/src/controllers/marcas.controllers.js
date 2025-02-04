a6251bf28cc4342f33de42ea5d4fc1eddfe1ab94
Veronica Prado Vargas <vprado@iriscx.com>
2025-02-03 20:11:16 -0400

    Adicionando: GetMarcabyId, updateMarcca, deleteMarca

--- backend/src/controllers/marcas.controllers.js
+++ backend/src/controllers/marcas.controllers.js
@@ -16,6 +16,21 @@ async function getMarcas(req, res) {
 
 }
 
+async function getMarcaById(req, res) {
+    try {
+        const id = req.params.id;
+        const marca = await Marca.findByPk(id, {
+            attributes: ['id', 'nombre_marca', 'updatedAt', 'createdAt'],
+        });
+        logger.info('Marca por Id', { marca });
+        return res.json(marca);
+    } catch (err) {
+        logger.error('Error getMarcaById', err);
+        res.status(500).json({ message: 'Error al obtener getMarcaById' });
+    }
+}
+
+
 async function createMarca(req, res) {
     try {
         const { nombre_marca } = req.body;
@@ -28,7 +43,42 @@ async function createMarca(req, res) {
     }
 }
 
+async function updateMarca(req, res) {
+    const id = req.params.id;
+    const { nombre_marca } = req.body;
+    if (!nombre_marca) return res.status(400).json({ message: 'nombre_marca es requerido' });
+    try {
+        const marca = await Marca.findByPk(id)
+        if (!marca) { return res.status(404).json({ message: 'marca no encontrada' }); }
+        marca.nombre_marca = nombre_marca;
+        await marca.save();
+        res.json(marca)
+
+    } catch (error) {
+        logger.error(`Error al actualizar marca: ${error.message}`);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+async function deleteMarca(req, res) {
+    const id = req.params.id;
+    try {
+        const marca = await Marca.findByPk(id)
+        if (!marca) { return res.status(404).json({ message: 'Marca no encontrada' }); }
+        await marca.destroy();
+        res.json({ message: 'Marca eliminada.' });
+
+    } catch (error) {
+        logger.error(`Error al eliminar marca: ${error.message}`);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
 export default {
     getMarcas,
-    createMarca
+    createMarca,
+    getMarcaById,
+    updateMarca,
+    deleteMarca,
+
 };
