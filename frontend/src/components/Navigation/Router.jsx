a59664b30bdb9c19e3d30dfaee0a52b2ab8f5694
Luchitu88 <luis.iturricha@gmail.com>
2025-02-08 13:19:52 -0400

    Se agregan funcion de Materiales y Proveedores

--- frontend/src/components/Navigation/Router.jsx
+++ frontend/src/components/Navigation/Router.jsx
@@ -5,6 +5,8 @@ import About from "../../components/Screens/About";
 import Marca from "../../components/Screens/Marca";
 import Categoria from "../../components/Screens/Categoria";
 import Unidad from "../../components/Screens/Unidad";
+import Material from "../../components/Screens/Material";
+import Proveedor from "../../components/Screens/Proveedor";
 
 const Router = () => {
   return (
@@ -12,9 +14,11 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/material" element={<Material />} />
         <Route path="/marca" element={<Marca />} />
         <Route path="/categoria" element={<Categoria />} />
         <Route path="/unidad" element={<Unidad />} />
+        <Route path="/proveedor" element={<Proveedor />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
