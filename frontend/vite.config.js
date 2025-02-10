99a57ffd6e0c47e018e628df976e52909078081a
ajmedinabalboa <alvaro.medina.developer@gmail.com>
2025-02-10 14:09:25 -0400

    change port configuration at frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 8383,
     host: true,
     watch: {
        usePolling: true,
